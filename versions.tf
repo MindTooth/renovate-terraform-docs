ecd1c6b1ee2b6d7e09f4a4404520d612bf95b928
Birger J. Nordølum <contact@mindtooth.no>
2023-09-11 11:11:52 +0200

    feat: add terraform

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.2"
+
+  required_providers {
+    dns = {
+      source  = "hashicorp/dns"
+      version = "2.2.0"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "1.24.0"
+    }
+  }
+}
